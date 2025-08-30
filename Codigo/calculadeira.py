153c55fcb866aff835aa4ceb39ec8c43921d5468
Ryan <ryanjobs234@gmail.com>
2025-08-29 21:42:20 -0300

    feat(calculadeira): adição do texto de agradecimento

--- Codigo/calculadeira.py
+++ Codigo/calculadeira.py
@@ -1,25 +1,17 @@
-continuar = "s"
+operacao = input("Qual operação deseja realizar? (+, -, *, /): ")
 
-while (continuar == "s"): 
-    operacao = input("Qual operação deseja realizar? (+, -, *, /): ")
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
 
-    num1 = float(input("Digite o primeiro número: "))
-    num2 = float(input("Digite o segundo número: "))
+if operacao == "+":
+    print(num1 + num2)
+elif operacao == "-":
+    print(num1 - num2)
+elif operacao == "*":
+    print(num1 * num2)
+elif operacao == "/":
+    print(num1 / num2)
+else:
+    print("Operação inválida")
 
-    if operacao == "+":
-        print(num1 + num2)
-    elif operacao == "-":
-        print(num1 - num2)
-    elif operacao == "*":
-        print(num1 * num2)
-    elif operacao == "/":
-        print(num1 / num2)
-    else:
-        print("Operação inválida")
-        
-    continuar = input("Deseja realizar outra operação? (s/n): ")
-    
-    if continuar == "n":
-        break
-
-print("Obrigado(a) por utilizar nosso programa!")
+print("Obrigado(a) por utilizar nosso programa. Volte sempre! ;)")
