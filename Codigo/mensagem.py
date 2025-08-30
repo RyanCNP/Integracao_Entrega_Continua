9bba03da01e71698ae276045bad79e263feac7bf
Ryan <ryanjobs234@gmail.com>
2025-08-29 21:56:59 -0300

    fix(mensagem): correção da repetição ilimitada de mensagens

--- Codigo/mensagem.py
+++ Codigo/mensagem.py
@@ -1,4 +1,10 @@
-n = int(input('Quantas vezes deseja exibir a mensagem?\n '))
+continuar = "s"
+while continuar == "s":
+    n = int(input('Quantas vezes deseja exibir a mensagem?\n '))
 
-for i in range(n):
-    print("Hello World")
+    for i in range(n):
+        print("Hello World")
+        
+    continuar = input("Deseja continuar? (s/n)")
+
+print("Obrigado(a) por utilizar nosso programa. Volte sempre! ;)")
