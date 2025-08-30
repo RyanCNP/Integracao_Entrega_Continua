97a513dae6bc95fb5120142bccec38575649d9a9
Ryan <ryanjobs234@gmail.com>
2025-08-29 21:38:57 -0300

    feat(calculadeira): adição do loop para o usuário não precisar rodar o programa toda vez que desejar realizar uma conta

--- Codigo/calculadeira.py
+++ Codigo/calculadeira.py
@@ -1,17 +1,25 @@
-operacao = input("Qual operação deseja realizar? (+, -, *, /): ")
+continuar = "s"
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+while (continuar == "s"): 
+    operacao = input("Qual operação deseja realizar? (+, -, *, /): ")
 
-if operacao == "+":
-    print(num1 + num2)
-elif operacao == "-":
-    print(num1 - num2)
-elif operacao == "*":
-    print(num1 * num2)
-elif operacao == "/":
-    print(num1 / num2)
-else:
-    print("Operação inválida")
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    if operacao == "+":
+        print(num1 + num2)
+    elif operacao == "-":
+        print(num1 - num2)
+    elif operacao == "*":
+        print(num1 * num2)
+    elif operacao == "/":
+        print(num1 / num2)
+    else:
+        print("Operação inválida")
+        
+    continuar = input("Deseja realizar outra operação? (s/n): ")
+    
+    if continuar == "n":
+        break
 
 print("Obrigado(a) por utilizar nosso programa!")
